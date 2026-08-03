6a188feff5237dce05530f1b12768cba6c2bba47
Humphrey Joshua <joshua@bemamusic.com>
2026-08-03 10:12:24 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     watch: {
-      ignored: ['**/data/*.sqlite', '**/data/*.sqlite-shm', '**/data/*.sqlite-wal'],
+      ignored: [
+        "**/data/*.sqlite",
+        "**/data/*.sqlite-shm",
+        "**/data/*.sqlite-wal",
+      ],
     },
   },
+  base: "./",
   build: {
     rollupOptions: {
       input: {
-        presentation: resolve(import.meta.dirname, 'index.html'),
-        control: resolve(import.meta.dirname, 'control.html'),
+        presentation: resolve(import.meta.dirname, "index.html"),
+        control: resolve(import.meta.dirname, "control.html"),
       },
     },
   },
-})
+});
